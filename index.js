f7fb284107c7c8f48114d65a6a69c47d6bad5567
Akeem Lawrence <akeemwlawrence@gmail.com>
2022-10-23 11:58:01 -0400

    Our first live coding commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    const newArray = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+    return newArray
+}
+
+function fuzzyMatch(drivers, string) {
+
+
+    const newArray = drivers.filter(driver => driver.substring(1,3) === string.substring(1,3));
+    return newArray;
+}
