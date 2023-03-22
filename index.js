77e6cb7ec716e13defd232d11d9123646b577fab
Akeem Lawrence <akeemwlawrence@gmail.com>
2023-03-22 13:40:13 -0400

    commit tree fe4e4b6c056045070a548c14bd4b22851c7fcb03

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 function findMatching(drivers, string) {
     const newArray = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
-    return newArray
+    return newArray;
 }
 
 function fuzzyMatch(drivers, string) {
-    const newArray = drivers.filter(driver => driver.startsWith(string));
+    const newArray = drivers.filter(driver => driver.startsWith(string))
     return newArray;
 }
 
 function matchName(drivers, string) {
-    const newArray = drivers.filter(drivers => drivers.name === string);
+    const newArray = drivers.filter(driver => driver.name === string )
     return newArray;
 }
-
